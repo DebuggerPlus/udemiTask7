379c93bcb636de80c6d4b6cdb83c346e97dc64d4
Jaguar <businessdeleu@gmail.com>
2020-05-31 12:22:57 +0300

    firstDOM

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let start = document.getElementById('start'),
+    resulttable = document.getElementsByClassName('result-table'),
+    expensesitem = document.getElementsByClassName('expenses-item'),
+    eibtn = document.getElementsByTagName('button')[0],
+    oebtn = document.getElementsByTagName('button')[1],
+    cbbtn = document.getElementsByTagName('button')[2],
+    optm = document.querySelectorAll('.optionalexpenses-item'),
+    cil = document.querySelector('.choose-income-label'),
+    svn = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    daybudget = document.querySelector('.daybudget'),
+    monthsavings = document.querySelector('.monthsavings'),
+    earsavings = document.querySelector('.yearsavings');
+
+
+console.log(start);
+console.log(resulttable);
